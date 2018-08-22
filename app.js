4af2754de0f7634e45b2d9c6cdac5bfd496d7fe1
jonhks <jonathan@laboratoria.la>
2018-08-21 19:57:06 -0500

    terminando basico vue

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+Vue.component('curso', {
+  props: ['curso'],
+  methods:{
+    onchange: function (ev){
+      // alert('detecta el cambio de check');
+      console.log(this.curso.value, ev.target.checked)
+      this.$emit('checked', this.curso.value, ev.target.checked)
+    }
+  },
+  template: `
+  <div>
+  <input type="checkbox" v-bind:id="curso.value" v-bind:value="curso.value" @change = "onchange">
+        <label  v-bind:for="curso.value">{{ curso.nombre }}</label>
+        <br>
+    </div>`
+})
+
 let app = new Vue({
   el: '#app',
   data: {
@@ -9,8 +26,7 @@ let app = new Vue({
     titulo: ' con vue',
     subtitulo: 'Creando un subtitulo',
     nombreObtenido: '',
-    cursos: [
-      {
+    cursos: [{
         nombre: 'Curso de Vue.js',
         value: 'vue'
       },
@@ -23,36 +39,44 @@ let app = new Vue({
         value: 'angular'
       },
       {
-      nombre: 'Curso de JQuery',
-      value: 'jquery'
+        nombre: 'Curso de JQuery',
+        value: 'jquery'
       },
       {
-      nombre: 'Curso de Js Es6', 
-      value: 'es6'
+        nombre: 'Curso de Js Es6',
+        value: 'es6'
       }
     ],
     cursosSeleccionados: []
-  }, 
+  },
   methods: {
     sumar: function () {
-       this.contador++
+      this.contador++
     },
-    restar: function  ()  {
-       this.contador--
+    restar: function () {
+      this.contador--
     },
     submit: function () {
       console.log(this.cursosSeleccionados)
+    },
+    selectCurso: function (curso, checked) {
+      if (checked) {
+        this.cursosSeleccionados.push(curso);
+      } else {
+        let index = this.cursosSeleccionados.indexOf(curso);
+        this.cursosSeleccionados.splice(index, 1);
+      }
     }
   },
   filters: {
     uppercase: str => {
       return str.toUpperCase();
-    }, 
+    },
     lowercase: str => {
       return str.toLowerCase();
     },
     capitalize: str => {
-      let firstLetter = str.charAt(0).toUpperCase() + str.slice(1) ;
+      let firstLetter = str.charAt(0).toUpperCase() + str.slice(1);
       return firstLetter;
     }
   }
